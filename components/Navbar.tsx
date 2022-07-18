4e7e025acbdd9eac2591aa38673d38a6b8a4b76d
Maestro1982 <kris.tote@telenet.be>
2022-07-18 16:36:22 +0200

    fourth commit

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -29,7 +29,7 @@ const Navbar = () => {
         {userProfile ? (
           <div className='flex gap-5 md:gap-10'>
             <Link href='/upload'>
-              <button className='border-2 px-2 md:px-4 text-md font-semibold flex items-center gap-2'>
+              <button className='border-2 px-2 md:px-4 text-md font-semibold flex items-center gap-2 hover:border-pink-500 hover:bg-[#F51997] hover:text-white'>
                 <IoMdAdd className='text-xl' />{' '}
                 <span className='hidden md:block'>Upload</span>
               </button>
